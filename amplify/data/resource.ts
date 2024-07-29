eeb9e067d194114b8634d779f14efad514e38c06
daniel <daniel21dev@gmail.com>
2024-07-28 18:36:09 -0600

    added per-user data isolation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -11,19 +11,20 @@ const schema = a.schema({
     .model({
       content: a.string(),
     })
-    .authorization((allow) => [allow.publicApiKey()]),
-});
+    .authorization((allow) => [allow.owner()]),
+})
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    // defaultAuthorizationMode: "apiKey",
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
+    defaultAuthorizationMode: 'userPool',
   },
 });
 
@@ -32,7 +33,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,9 @@ function App() {
   return (
 
     <Authenticator>
-      {({ signOut }) => (
+      {({ signOut, user }) => (
         <main>
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>
           <h1>My todos</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
